bb3866ea0eec461bf23abd785fac3b3178bf2fb3
kmadhav907 <kmadhav907@gmail.com>
2021-04-09 18:19:17 +0530

    Added Slug Pages for each Item

--- components/Layout.js
+++ components/Layout.js
@@ -1,27 +1,25 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Layout({ children }) {
   return (
-    <div className="layout">
+    <div className='layout'>
       <header>
-        <Link href="/">
+        <Link href='/'>
           <a>
             <h1>
               <span>Just Add</span>
-              <span>Marmite</span>
+              <span>FoodBlog</span>
             </h1>
             <h2>Spread The Joy</h2>
           </a>
         </Link>
       </header>
 
-      <div className="page-content">
-        { children }
-      </div>
+      <div className='page-content'>{children}</div>
 
       <footer>
-        <p>Copyright 2021 Just Add Marmite :)</p>
+        <p>Copyright 2021 Just Add Food in your Tummy :)</p>
       </footer>
     </div>
-  )
+  );
 }
